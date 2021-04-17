8f93b192fd27aff54bfd3a4e01b9dcffc7473c5c
Виталий Сливец <123@unet.by>
2021-04-17 19:09:29 +0300

    add

---
+++ src/ArrayScan.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayScan {
+    public static void main(String[] args){
+        Scanner scann = new Scanner(System.in);
+        System.out.println("Введите число");
+        int scanNumber = scann.nextInt();
+        int[] arrayNumber = {13,2,5,3,17,14};
+            for (int i : arrayNumber) {
+                if (scanNumber == i) {
+                    System.out.println("Массив содержит число " + i);
+                return;
+                }
+
+            }System.out.println("Массив не содержит число " + scanNumber);
+    }
+
+}
